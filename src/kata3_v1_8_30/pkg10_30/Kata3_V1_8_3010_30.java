2b01fe600b86e9984384e6d16f3df66789cbc03d
gatete <gatete@DESKTOP-OJJTRN4>
2022-10-19 10:42:57 +0200

    Kata 3 version 2

--- src/kata3_v1_8_30/pkg10_30/Kata3_V1_8_3010_30.java
+++ src/kata3_v1_8_30/pkg10_30/Kata3_V1_8_3010_30.java
@@ -3,7 +3,18 @@ package kata3_v1_8_30.pkg10_30;
 public class Kata3_V1_8_3010_30 {
 
     public static void main(String[] args) {
-        histogramDisplay display = new histogramDisplay();
+        Histogram<String> histogram = new Histogram<>();
+
+        
+        histogram.increment("ulplgc.es");
+        histogram.increment("dis.ulplgc.es");
+        histogram.increment("ulplgc.es");
+        histogram.increment("dis.ulplgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        
+        histogramDisplay display = new histogramDisplay(histogram, "Histogram display");
     }
     
 }
